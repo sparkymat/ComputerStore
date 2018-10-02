fbe137512d73d549088596acb1738e896b785200
Ajith Hussain <csy0013@googlemail.com>
2018-10-02 20:39:59 +1000

    Added logic for x for y

--- app/pricing_rule.rb
+++ app/pricing_rule.rb
@@ -1,13 +1,42 @@
 # frozen_string_literal: true
 
 class PricingRule
+  RULE_TYPES = %i(
+    x_for_y
+    bulk_discount
+    free_items
+  ).freeze
+
   def initialize(rule_type:, trigger_value:, deal_value:, deal_item:)
-    @rule_type = rule_type
+    @rule_type     = rule_type
     @trigger_value = trigger_value
-    @deal_value = deal_value
-    @deal_item = deal_item
+    @deal_value    = deal_value
+    @deal_item     = deal_item
   end
 
   def apply(sku:, price_cents:, count:)
+    case @rule_type
+    when :x_for_y
+      items = []
+      items << { sku: sku, count: count }
+      apply_times = count / @trigger_value
+      remaining = count % @trigger_value
+
+      deal_cents = apply_times * @deal_value * price_cents
+      remaining_cents = remaining * price_cents
+
+      total_cents = deal_cents + remaining_cents
+
+      result = {
+        items:       items,
+        total_cents: total_cents,
+      }
+    when :bulk_discount
+    when :free_items
+    else
+      raise 'unknown rule type'
+    end
+
+    result
   end
 end
